24084e8957b31b1ff7baf6b222474dacfea3bcdb
Frank Goossen <frank@xprtz.net>
2025-05-06 16:18:16 +0200

    Update to pizza party

--- NBomber/NBomberTestConsole/Program.cs
+++ NBomber/NBomberTestConsole/Program.cs
@@ -1,52 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using NBomber.CSharp;
 using NBomber.Contracts;
-using NBomber.Contracts.Stats;
 
-Console.WriteLine("Press any key to start the load test...");
-Console.ReadLine();
+Console.WriteLine("Starting load test...");
 
-var httpClient = new HttpClient
-{
-    BaseAddress = new Uri("http://localhost:5246") // Use HTTP to avoid HTTPS cert issues
-};
+var httpClient = new HttpClient();
 
-// Scenario 1: Weather Forecast API (localhost:5246/weatherforecast)
-var weatherForecastScenario = Scenario.Create("weatherforecast_scenario", async context =>
-    {
-        try
-        {
-            var response = await httpClient.GetAsync("/weatherforecast");
-            var isSuccess = response.IsSuccessStatusCode;
-
-            var sizeBytes = response.Content.Headers.ContentLength;
-
-            return isSuccess ? Response.Ok(response.StatusCode, sizeBytes: sizeBytes ?? 0) : Response.Fail(response.StatusCode, sizeBytes: sizeBytes ?? 0);
-        }
-        catch (Exception ex)
-        {
-            return Response.Fail(message: ex.Message);
-        }
-    })
-    .WithoutWarmUp()
-    .WithLoadSimulations(
-        Simulation.Inject(rate: 5, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(10)),
-        Simulation.RampingInject(rate: 5, interval: TimeSpan.FromMilliseconds(100), during: TimeSpan.FromSeconds(5))
-    );
-
-// Scenario 2: JSON Placeholder API (jsonplaceholder.typicode.com/posts/1)
-var jsonPlaceholderScenario = Scenario.Create("json_placeholder", async context =>
+// Define scenario for GET https://quickpizza.grafana.com/
+var quickPizzaScenario = Scenario.Create("quickpizza_homepage", async context =>
     {
         var sw = Stopwatch.StartNew();
 
         try
         {
-            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/1");
+            var response = await httpClient.GetAsync("https://quickpizza.grafana.com/");
             sw.Stop();
 
             var contentLength = response.Content.Headers.ContentLength ?? 0;
@@ -55,11 +25,11 @@ var jsonPlaceholderScenario = Scenario.Create("json_placeholder", async context 
                 ? Response.Ok(
                     statusCode: ((int)response.StatusCode).ToString(),
                     sizeBytes: contentLength,
-                    message: "Success!",
+                    message: "Homepage Success",
                     customLatencyMs: sw.Elapsed.TotalMilliseconds)
                 : Response.Fail(
                     statusCode: ((int)response.StatusCode).ToString(),
-                    message: "Request failed",
+                    message: "Homepage Failed",
                     sizeBytes: contentLength,
                     customLatencyMs: sw.Elapsed.TotalMilliseconds);
         }
@@ -73,44 +43,12 @@ var jsonPlaceholderScenario = Scenario.Create("json_placeholder", async context 
     })
     .WithoutWarmUp()
     .WithLoadSimulations(
-        Simulation.Inject(rate: 5, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(20)));
-
-// Scenario 3: Slow API (httpbin.org/delay/1)
-var httpBinScenario = Scenario.Create("httpbin_delay", async context =>
-    {
-        var sw = Stopwatch.StartNew();
-
-        try
-        {
-            var response = await httpClient.GetAsync("https://httpbin.org/delay/1");
-            sw.Stop();
-
-            return response.IsSuccessStatusCode
-                ? Response.Ok(
-                    statusCode: ((int)response.StatusCode).ToString(),
-                    message: "Delay Success",
-                    customLatencyMs: sw.Elapsed.TotalMilliseconds)
-                : Response.Fail(
-                    statusCode: ((int)response.StatusCode).ToString(),
-                    message: "Delay Failed",
-                    customLatencyMs: sw.Elapsed.TotalMilliseconds);
-        }
-        catch (Exception ex)
-        {
-            sw.Stop();
-            return Response.Fail<string>(
-                message: $"Exception: {ex.Message}",
-                customLatencyMs: sw.Elapsed.TotalMilliseconds);
-        }
-    })
-    .WithoutWarmUp()
-    .WithLoadSimulations(
-        Simulation.Inject(rate: 2, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(20)));
-
-
+        Simulation.Inject(rate: 10, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(10)),
+        Simulation.RampingInject(rate: 30, interval: TimeSpan.FromMilliseconds(250), during: TimeSpan.FromSeconds(10))
+    );
 
 NBomberRunner
-    .RegisterScenarios(weatherForecastScenario, jsonPlaceholderScenario, httpBinScenario)
+    .RegisterScenarios(quickPizzaScenario)
     .Run();
 
 Console.WriteLine("Load test completed. Press any key to exit...");
