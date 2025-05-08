5e85e3342a6dcb20edac8a7ee40346c1763fb68d
Frank Goossen <frank@xprtz.net>
2025-05-08 19:09:43 +0200

    Add failing test

--- NBomber/NBomberXUnitTests/QuickPizzaTests.cs
+++ NBomber/NBomberXUnitTests/QuickPizzaTests.cs
@@ -84,7 +84,7 @@ namespace NBomberXUnitTests
                 .LoadInfraConfig(infraConfigPath)
                 .Run();
 
-            var scenarioStats = stats.ScenarioStats.FirstOrDefault(s => s.ScenarioName == "quickpizza_perf");
+            var scenarioStats = stats.ScenarioStats.FirstOrDefault(s => s.ScenarioName == "quickpizza_homepage");
             Assert.NotNull(scenarioStats);
 
             var stepStats = scenarioStats.StepStats.FirstOrDefault(s => s.StepName == "step_1");
@@ -97,6 +97,85 @@ namespace NBomberXUnitTests
             _outputHelper.WriteLine($"Failure rate: {failRate:P2}");
 
             Assert.True(p95 < 2000, $"P95 latency too high: {p95} ms");
+            Assert.True(failRate < 0.02, $"Failure rate too high: {failRate:P2}");
+        }
+
+        [Fact]
+        public void QuickPizza_Homepage_Should_Fail_If_Too_Slow()
+        {
+            var httpClient = new HttpClient();
+
+            var isCi = Environment.GetEnvironmentVariable("CI") == "true";
+            string infraConfigPath;
+
+            var token = Environment.GetEnvironmentVariable("INFLUXDB_TOKEN");
+
+            if (!isCi && string.IsNullOrWhiteSpace(token))
+            {
+                var config = new ConfigurationBuilder()
+                    .AddUserSecrets<QuickPizzaTests>()
+                    .Build();
+                token = config["Token"];
+            }
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new Exception("InfluxDB token was not found in env var or user secrets.");
+            }
+
+            var json = File.ReadAllText("infra-config.json");
+            json = json.Replace("\"Token\": \"REPLACE_ME\"", $"\"Token\": \"{token}\"");
+
+            infraConfigPath = Path.Combine(Path.GetTempPath(), "patched-infra-config.json");
+            File.WriteAllText(infraConfigPath, json);
+
+            var influxDbSink = new InfluxDBSink();
+
+            var scenario = Scenario.Create("quickpizza_homepage", async context =>
+            {
+                var step = await Step.Run("step_1", context, async () =>
+                {
+                    var request = Http.CreateRequest("GET", "https://quickpizza.grafana.com");
+                    var response = await Http.Send(httpClient, request);
+                    return response;
+                });
+
+                return step;
+            })
+                .WithWarmUpDuration(TimeSpan.FromSeconds(1))
+                .WithLoadSimulations(
+                    Simulation.RampingInject(rate: 0, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(10)),
+                    Simulation.Inject(rate: 20, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(15)),
+                    Simulation.RampingInject(rate: 20, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(10))
+                );
+
+            var postFix = $"XUNIT-{(isCi ? "CI" : "MANUAL")}";
+            var sessionId = $"{DateTime.UtcNow:yyyyMMdd-HHmmss}-{postFix}";
+            _outputHelper.WriteLine($"[NBomber] Writing reports to: {Path.GetFullPath("reports")}");
+
+            var stats = NBomberRunner
+                .RegisterScenarios(scenario)
+                .WithSessionId(sessionId)
+                .WithReportFolder("reports")
+                .WithReportFormats(ReportFormat.Html, ReportFormat.Md)
+                .WithReportingInterval(TimeSpan.FromSeconds(5))
+                .WithReportingSinks(influxDbSink)
+                .LoadInfraConfig(infraConfigPath)
+                .Run();
+
+            var scenarioStats = stats.ScenarioStats.FirstOrDefault(s => s.ScenarioName == "quickpizza_homepage");
+            Assert.NotNull(scenarioStats);
+
+            var stepStats = scenarioStats.StepStats.FirstOrDefault(s => s.StepName == "step_1");
+            Assert.NotNull(stepStats);
+
+            var p95 = stepStats.Ok.Latency.Percent95;
+            var failRate = scenarioStats.AllFailCount / (double)scenarioStats.AllRequestCount;
+
+            _outputHelper.WriteLine($"P95 latency: {p95} ms");
+            _outputHelper.WriteLine($"Failure rate: {failRate:P2}");
+
+            Assert.True(p95 < 20, $"P95 latency too high: {p95} ms");
             Assert.True(failRate < 0.02, $"Failure rate too high: {failRate:P2}");
         }
     }
